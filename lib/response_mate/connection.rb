92b8e386c3bae9f697df99607b2ae0cd23217627
Dimitris Zorbas <zorbash@skroutz.gr>
2013-12-24 15:52:34 +0200

    Extract Connection and Tape as separate classes

---
+++ lib/response_mate/connection.rb
@@ -0,0 +1,34 @@
+class ResponseMate::Connection
+
+  delegate :params, to: :client
+  delegate(*(ResponseMate::ManifestParser::HTTP_VERBS), to: :client)
+
+  attr_accessor :client, :base_url
+
+  def initialize(base_url)
+    @base_url = base_url
+    @client = Faraday.new do |c|
+      c.use FaradayMiddleware::FollowRedirects, limit: 5
+      c.adapter Faraday.default_adapter
+    end
+
+    client.headers.merge(ResponseMate::DEFAULT_HEADERS)
+    client.url_prefix = base_url if base_url
+  end
+
+  def fetch(request)
+    client.params = request[:params] if !request[:params].nil?
+    client.send request[:verb].downcase.to_sym, "#{base_url}#{request[:path]}"
+  rescue Faraday::Error::ConnectionFailed
+    puts "Is a server up and running at #{request[:path]}?".red
+    exit 1
+  end
+
+  def set_headers_from_manifest(manifest)
+    if manifest.try(:[], 'default_headers')
+      manifest['default_headers'].each_pair do |k, v|
+        client.headers[ResponseMate::Helpers.headerize(k)] = v
+      end
+    end
+  end
+end
