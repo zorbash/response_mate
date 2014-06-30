e2b0e07411dc9c50963ae276dfb2d96a0bbbcd12
Dimitris Zorbas <zorbash@skroutz.gr>
2014-07-01 01:40:02 +0300

    Refactor ResponseMate::Commands::Inspect

--- spec/lib/response_mate/commands/inspect_spec.rb
+++ spec/lib/response_mate/commands/inspect_spec.rb
@@ -6,18 +6,56 @@ describe ResponseMate::Commands::Inspect do
   include_context 'stubbed_requests'
 
   describe '#run' do
-    context 'when not in interactive mode' do
-      before do
-        ResponseMate::Commands::Inspect.new(['user_issues'], {}).run
-      end
+    context 'when no keys are specified' do
+      it 'displays help message' do
+        error_output = nil
 
-      it 'koko' do
+        quietly do
+          error_output = capture(:stderr) do
+            ResponseMate::Commands::Inspect.new([], {}).run
+          end
+        end
 
+        expect(error_output.strip).to eq("At least one key has to be specified".red)
       end
     end
 
-    context 'when in interactive mode' do
+    context 'when key is specified' do
+      let(:mock_request) do
+        ResponseMate::Request.new(key: 'user_issues',
+                                  request: {
+                                    verb: 'GET',
+                                    host: 'www.someapi.com',
+                                    path: '/user/42/issues' }).normalize!
+      end
 
+      let(:captured_output) do
+        output = nil
+
+        quietly do
+          output = capture(:stdout) do
+            ResponseMate::Commands::Inspect.new(['user_issues'], {}).run
+          end
+        end
+        output.strip
+      end
+
+      it 'displays the key, host, path, verb of the request' do
+        expect(captured_output).to include(mock_request.to_cli_format)
+      end
+
+      it 'includes the response status' do
+        expect(captured_output).
+          to include(":status => #{fake_response_user_issues[:status].to_s}")
+      end
+
+      it 'includes the response body' do
+        expect(captured_output).to include(%(:body => "#{fake_response_user_issues[:body]}"))
+      end
+
+      it 'includes the response headers' do
+        expect(captured_output).to include(fake_response_user_issues[:headers].values.last)
+      end
     end
   end
 end
