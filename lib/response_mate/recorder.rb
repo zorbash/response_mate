f4ee62d5df55412c93491923d420a418d4110b15
Dimitris Zorbas <zorbash@skroutz.gr>
2014-07-02 01:57:44 +0300

    Cleanup ResponseMate::Recorder

--- lib/response_mate/recorder.rb
+++ lib/response_mate/recorder.rb
@@ -9,38 +9,22 @@ module ResponseMate
 
     def initialize(args = {})
       @manifest = args[:manifest]
-      @keys = args[:keys]
       @conn = ResponseMate::Connection.new
     end
 
-    def record
-      requests = manifest.requests
+    def record(keys = :all)
+      requests = keys == :all ? manifest.requests : manifest.requests_for_keys(keys)
 
-      if keys.present?
-        existing_keys = requests.map(&:key)
-        missing_keys = keys - existing_keys
-
-        if missing_keys.present?
-          raise ResponseMate::KeysNotFound.new(missing_keys.join(','))
-        end
-
-        requests.select! do |r|
-          keys.include? r.key
-        end
-      end
-
-      requests.each do |request|
-        process request.key, request
-      end
+      requests.each { |request| process request }
     end
 
     private
 
-    def process(key, request)
+    def process(request)
       meta = request.meta
       puts request.to_cli_format
 
-      ResponseMate::Tape.new.write(key, request, conn.fetch(request), meta)
+      ResponseMate::Tape.new.write(request.key, request, conn.fetch(request), meta)
     end
   end
 end
