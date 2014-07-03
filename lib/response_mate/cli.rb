ddff327464df1a0289035a7232ec7c1db4afecab
Dimitris Zorbas <zorbash@skroutz.gr>
2014-07-03 15:44:20 +0300

    Remove interactive option from inspect command

--- lib/response_mate/cli.rb
+++ lib/response_mate/cli.rb
@@ -15,7 +15,6 @@ module ResponseMate
 
     desc 'inspect [key1,key2]', 'Perform requests and print their output'
     method_option :requests_manifest, aliases: '-r'
-    method_option :interactive, type: :boolean, aliases: '-i'
     def inspect(*keys)
       ResponseMate::Commands::Inspect.new(args, options).run
     end
