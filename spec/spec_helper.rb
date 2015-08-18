af2bc6e6fb0cd38c1d8e148eab219f29962ec9b5
Dimitris Zorbas <zorbash@skroutz.gr>
2015-08-18 17:02:28 +0300

    Ignore Kernel#quietly deprecation for tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,6 +7,19 @@ Coveralls.wear!
 
 Dir['./spec/support/**/*.rb'].sort.each { |f| require f }
 
+module OutputHelpers
+  # Deprecated in active_support due to thread-safety
+  # see: https://github.com/rails/rails/blob/v4.2.2/activesupport/lib/active_support/core_ext/kernel/reporting.rb#L114
+  # We use it only as a testing helper in a non-threaded environment
+  def quietly
+    silence_stream(STDOUT) do
+      silence_stream(STDERR) do
+        yield
+      end
+    end
+  end
+end
+
 RSpec.configure do |c|
-  c.treat_symbols_as_metadata_keys_with_true_values = true
+  c.include OutputHelpers
 end
